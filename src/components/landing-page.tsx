9b63f49051085c68ecb30d409a2ab8d89dfe33f0
aishtiaq <awshafishtiaque@gmail.com>
2024-10-23 02:47:10 -0700

    4k raw playing from public folder

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -33,7 +33,8 @@ export function LandingPage() {
           loop
           playsInline
         >
-          <source src="placeholder.mp4" type="video/mp4" />
+          {/* <source src="placeholder.mp4" type="video/mp4" /> */}
+          <source src="new4k.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
